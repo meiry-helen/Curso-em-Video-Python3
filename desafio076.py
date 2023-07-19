748fe85c64a972573a3df69316c0b3250be54bb7
meiry-helen <meiry_helen@yahoo.com.br>
2023-07-19 19:30:43 -0300

    Exercícios sobre tuplas.

--- desafio076.py
+++ desafio076.py
@@ -1,2 +1,17 @@
 '''Crie um programa que tenha uma tupla única com nomes de produtos e seus respectivos preços na sequência.
 No final, mostre uma listagem de preços, organizando os dados em forma tabular.'''
+
+produtos = ('Computador', 2500,
+            'Televisão', 1900.50,
+            'Geladeira', 2690.60,
+            'Liquidificador', 250.20,
+            'Mesa', 900.98)
+print('-'*40)
+print(f'{"LISTAGEM DE PREÇOS":^35}')
+print('-'*40)
+for pos in range(0, len(produtos)):
+    if pos % 2 == 0:
+        print(f'{produtos[pos]:.<30}', end='')
+    else:
+        print(f'R$ {produtos[pos]:>7.2f}')
+print('-'*40)
--- desafio077.py
+++ desafio077.py
@@ -1,2 +1,37 @@
 '''Crie um programa que tenha uma tupla com várias palavras (não usar acentos). Depois disso, você deve mostrar, para
 cada palavra, quais são as suas vogais.'''
+
+palavras = ('cadeira', 'cha', 'paralelepipedo', 'muqueca', 'professora')
+
+'''letra = ' '
+for i in palavras:
+    print(palavras[c], end='                ')
+    if 'a' in palavras[c]:
+        print('a', end=' ')
+    if 'e' in palavras[c]:
+        print('e', end=' ')
+    if 'i' in palavras[c]:
+        print('i', end=' ')
+    if 'o' in palavras[c]:
+        print('o', end=' ')
+    if 'u' in palavras[c]:
+        print('u', end=' ')
+    print('\n')
+    c += 1'''
+'''l = 0
+c = 0
+VOGAIS = 'aeiou'
+for i in palavras:
+    print(f'{palavras[c]}.......', end=' ')
+    for letra in VOGAIS:
+        if letra in palavras[c]:
+            print(letra, end=' ')
+        l += 1
+    print(sep=' ')
+    c += 1'''
+
+for p in palavras:
+    print(f'\nNa palavra {p.upper()} temos ',end='')
+    for letra in p:
+        if letra.lower() in 'aeiou':
+            print(letra, end=' ')
