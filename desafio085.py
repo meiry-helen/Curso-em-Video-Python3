5bf27bbc0c2a2b48e1e310644958a7ed64df6933
meiry-helen <meiry_helen@yahoo.com.br>
2023-07-31 18:38:58 -0300

    Exercícios sobre listas compostas.

--- desafio085.py
+++ desafio085.py
@@ -1,7 +1,7 @@
 '''Crie um programa onde os usuários possam digitar sete valores numéricos e cadastre-os em uma lista única que mantenha
 separados os valores pares e ímpares. No final, mostre os valores pares e ímpares em ordem crescente.'''
 
-numeros = [[],[]]
+numeros = [[], []]
 
 for c in range(0, 7):
     n = (int(input('Informe um número: ')))
--- desafio088.py
+++ desafio088.py
@@ -1,2 +1,18 @@
 '''Faça um programa que ajude um jogador da Mega Sena a criar palpites. O programa vai perguntar quantos jogos serão
 gerados e vai sortear 6 números entre 1 e 60 para cada jogo, cadastrando tudo em uma lista composta.'''
+from random import randint, sample
+
+n_jogos = int(input('Indique o número de jogos: '))
+tamanho = 6
+valores = range(0, 61)
+
+sorteio = []
+
+for i in range(n_jogos):
+    sorteio.append(sample(valores, tamanho))
+
+for i in range(n_jogos):
+    for c in range(tamanho):
+        print(sorteio[i][c], end=' ')
+    print()
+
